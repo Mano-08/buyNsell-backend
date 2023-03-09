cb6d9f7f67153a44c8c9eb21c119bf155e2107a2
amartarh <amartarh@gmail.com>
2023-03-09 11:40:33 +0530

    add and remove user

--- controller/buyNsellController.js
+++ controller/buyNsellController.js
@@ -1,4 +1,6 @@
 const User = require("../models/userSchema");
+const Product = require("../models/productSchema");
+const Users = require("../models/usersSchema");
 
 const login = (req, res, next) => {
   const { username, password, isAdmin } = req.body;
@@ -33,6 +35,37 @@ const login = (req, res, next) => {
     });
 };
 
+const creatU = (req, res, next) => {
+  const { userid, name, password, branch, phone, year, email, address } =
+    req.body;
+  const Users = new Users({
+    userid: Number,
+    name: String,
+    password: String,
+    branch: String,
+    phone: Number,
+    year: Number,
+    email: String,
+    address: String,
+  });
+  Users.save()
+    .then(() => {
+      res.status(200).json({ message: "User added success" });
+    })
+    .catch((error) => {
+      res.status(400).json({ message: "User adding failed" });
+    });
+};
+
+const removeU = async (req, res, next) => {
+  let id = req.body.userid;
+  await Users.findByIdAndDelete(id).catch(function (err) {
+    console.log(err);
+  });
+};
+
 module.exports = {
   login,
+  createU,
+  removeU,
 };
